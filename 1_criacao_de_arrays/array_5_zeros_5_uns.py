f61911e08ab3a15208f5fcc938913ce437f863e9
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-27 14:38:28 -0300

    .

---
+++ 1_criacao_de_arrays/array_5_zeros_5_uns.py
@@ -0,0 +1,10 @@
+# Crie um array de zeros com 5 elementos e outro de uns com 5 elementos.
+import numpy as np
+import os
+os.system('clear')
+
+array_zeros = np.zeros(5)
+array_uns = np.ones(5)
+
+print(array_zeros)
+print(array_uns)
