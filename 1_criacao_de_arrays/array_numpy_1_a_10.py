d6213932735236e30fdbe760f547b8894b4172ab
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-27 14:32:57 -0300

    .

---
+++ 1_criacao_de_arrays/array_numpy_1_a_10.py
@@ -0,0 +1,8 @@
+# Crie um array numpy com os números de 1 a 10 e exiba-o.
+import numpy as np
+import os
+os.system('clear')
+
+vetor_numpy = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+print(vetor_numpy)
