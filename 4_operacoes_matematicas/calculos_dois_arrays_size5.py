ea9b55f134359e3a7c10c5d213d1787f580b9cce
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-28 13:21:39 -0300

    .

--- 4_operacoes_matematicas/calculos_dois_arrays_size5.py
+++ 4_operacoes_matematicas/calculos_dois_arrays_size5.py
@@ -10,7 +10,14 @@ os.system('clear')
 numeros_um = np.random.randint(1, 11, size=5)
 numeros_dois = np.random.randint(1, 11, size=5)
 
-numeros_soma = numeros_um + numeros_dois
+numeros_soma = np.sum(numeros_um) + np.sum(numeros_dois)
+numeros_sub = np.sum(numeros_um) - np.sum(numeros_dois)
+numeros_mult = numeros_um * numeros_dois
 
 print(f'numeros_um={numeros_um}')
-print(f'numeros_dois={numeros_dois}')
+print(f'numeros_dois={numeros_dois}\n')
+
+print(f'numeros_mult={numeros_mult}')
+print(f'numeros_soma={numeros_soma}')
+print(f'numeros_sub={numeros_sub}\n')
+
