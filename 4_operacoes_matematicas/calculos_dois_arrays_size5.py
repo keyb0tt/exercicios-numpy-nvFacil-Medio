2a829e0ad3487cd902a4b106e5e5f01875822b97
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-28 13:24:56 -0300

    .

--- 4_operacoes_matematicas/calculos_dois_arrays_size5.py
+++ 4_operacoes_matematicas/calculos_dois_arrays_size5.py
@@ -13,11 +13,13 @@ numeros_dois = np.random.randint(1, 11, size=5)
 numeros_soma = np.sum(numeros_um) + np.sum(numeros_dois)
 numeros_sub = np.sum(numeros_um) - np.sum(numeros_dois)
 numeros_mult = numeros_um * numeros_dois
+numeros_div = numeros_um / numeros_dois
 
 print(f'numeros_um={numeros_um}')
 print(f'numeros_dois={numeros_dois}\n')
 
 print(f'numeros_mult={numeros_mult}')
+print(f'numeros_div={numeros_div}')
 print(f'numeros_soma={numeros_soma}')
 print(f'numeros_sub={numeros_sub}\n')
 
